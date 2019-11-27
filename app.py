961fbcecded662de8319084b59f4fbfbfde64fc9
Theo Thiedig <theo@thiedig.net>
2019-11-27 13:29:14 +0100

    Added commentary and logging

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
+import logging
+
 import gpio
 from flask import Flask, render_template, request
 
+# Flask server
 app = Flask(__name__)
 
+# Logging facility
+logger = logging.getLogger('LEDMatrix')
+logger.setLevel(logging.DEBUG)
+
+# GPIO init
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(True)
 
-led = 26
+led = 26  # GPIO 26 = yellow LED
 
 gpio.setup(led, gpio.OUT)
 gpio.output(led, gpio.LOW)
+logger.debug("GPIO initialized.")
 
 
 def changeText(text):
+    logger.debug("Changed text to: %s", text)
     pass
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    logger.debug("entered '/'")
     data = request.form['display']
     changeText(data)
     return render_template('index.html')
@@ -38,6 +49,7 @@ def action(device, status):
     if status == 'off':
         gpio.output(actuator, gpio.LOW)
     render_template('done.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
